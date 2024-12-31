b6460dbc53d7459dd5b115ac2a380681f2822816
venkat posina <venkatposina29@gmail.com>
2024-12-31 10:52:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// let students = [
+//     {id:1,s_name:"pavan",city:"hyd"},
+//     {id:2,s_name:"venkat",city:"sec"},
+//     {id:3,s_name:"meena",city:"vizag"},
+//     {id:4,s_name:"rahul",city:"benguluru"},
+//     {id:5,s_name:"sita",city:"chennai"},
+//     {id:6,s_name:"vijay",city:"mumbai"}
+// ];
+
+// // for(let i in students)
+// // {
+// //     console.log(students[i].s_name,`from`,students[i].city)
+// // }
+
+
+// for(let i of students)
+//     {
+//         console.log(`${i.s_name} from ${i.city}`)
+//     }
+
+
+const ven=[45,25,35,15]
+
+for(i in ven)
+{
+    console.log(+i+1,ven[i])
+}
